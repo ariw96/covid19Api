31d19dd94ade186ae6e1b3e14d840910b3461c9f
ariw96 <ariw96@gmail.com>
2022-05-15 04:00:00 +0300

    evantlistener

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const state = { continent: null, dataType: "confirmed" };
 const continentsList = ["asia", "europe", "africa", "america"];
 const countriesMap = {};
 const covidPerCountryMap = {};
+let latest_dataArr = [];
 const cors = "https://intense-mesa-62220.herokuapp.com/";
 const colors = { 1: "#6C4A4A", 2: "#C89595", 3: "#DDBEBE", 4: "#EDEDED" };
 
@@ -14,33 +15,6 @@ const chartContainerEl = document.querySelector(".chart-container");
 const openingAreaEl = document.querySelector(".main__opening");
 const spinnerEl = document.querySelector(".loading-spinner");
 
-//classes
-function Country(name, code) {
-    this.name = name;
-    this.code = code;
-}
-
-class CovidData {
-    constructor(continent, confirmed, deaths, recovered, critical) {
-        this.continent = continent;
-        this.confirmed = confirmed;
-        this.deaths = deaths;
-        this.recovered = recovered;
-        this.critical = critical;
-    }
-}
-
-
-//functions
-//check if data exists. if not, get it from api
-function getData(continent) {
-    if (countriesMap[continent]) {
-        return;
-    } else {
-        spinnerEl.classList.toggle("display-none");
-        getCountriesData(continent);
-    }
-}
 
 async function getCountriesData(continent) {
     try {
@@ -48,50 +22,97 @@ async function getCountriesData(continent) {
             `${cors}https://restcountries.herokuapp.com/api/v1/region/${continent}?fields=name;alpha2Code`
         );
         const { data } = countriesApi;
-        const curContinentCodeArr = countriesMap[continent] = data.map((country) => {
-            return (new Country(country.name.common, country.cca2));
+        const curContinentCodeArr = await data.map((country) => {
+            return country.cca2;
         })
-
-        const countriesPromiseArr = curContinentCodeArr.map((country) => {
-            return axios.get(`${cors}https://corona-api.com/countries/${country.code}`);
+        const curContinentNameArr = await data.map((country) => {
+            return country.name.common;
+        })
+        displayBtn(curContinentNameArr)
+        countriesMap[continent] = curContinentNameArr;
+        const countriesPromiseArr = curContinentCodeArr.map((code) => {
+            return axios.get(`${cors}https://corona-api.com/countries/${code}`);
         });
         const countriesDataArr = await Promise.allSettled(countriesPromiseArr);
-        const covidData = countriesDataArr.map((country) => {
-            const latestData = country.value.data.data.latest_data;
-            return latestData
+        const covidCountryMap = {}
+        latest_dataArr = countriesDataArr.map((element) => {
+            covidCountryMap[element.value.data.data.name] = element.value.data.data.latest_data
+            return element.value.data.data.latest_data
         })
-        console.log(data, curContinentCodeArr, countriesPromiseArr, countriesDataArr, covidData)
+        covidPerCountryMap[continent] = covidCountryMap
+
+
+
+        createChart(continent)
+
+        console.log(covidPerCountryMap[continent], latest_dataArr)
     } catch (error) {
         console.log(error);
     }
 }
-//countries and covid API's are somewhat inconsistent. fix it:
 
-getCountriesData("asia")
+function displayBtn(arr) {
+    arr.forEach((country) => {
+        const countryEl = document.createElement("button");
+        countryEl.classList.add("btn", "btn-country");
+        countryEl.dataset.country = country;
+        countryEl.innerText = country;
+        countriesContainerEl.appendChild(countryEl);
 
-// store data in variables
-
-//get covid data by countries and store in variables
-
-
-//fetch multiple simultaneously
-
-
-//countries and covid API's are somewhat inconsistent. fix it:
+    });
+}
 
 
-
-
-//create button for each country
 
 //create a line chart
+function createChart(continent) {
+    if (countriesMap[continent]) {
+        const chartEl = document.createElement("canvas");
+        chartContainerEl.innerHTML = "";
+        chartContainerEl.appendChild(chartEl);
+        chartEl.setAttribute(
+            "height",
+            (window.screen.availHeight / 3.5).toString()
+        );
+        chartEl.setAttribute("width", (window.screen.availHeight * 0.8).toString());
+        Chart.defaults.global.defaultFontColor = colors[1];
+        const chart = new Chart(chartEl, {
+            type: "line",
+            data: {
+                labels: countriesMap[continent],
+                datasets: [{
+                    data: dataTypeArr(state.dataType),
+                    backgroundColor: "rgba(200, 149, 149, 0.2)",
+                    borderColor: colors[2],
+                }, ],
+            },
+            options: {
+                title: {
+                    display: true,
+                    text: `Covid-19 in ${continent}`,
+                    fontSize: 20,
+                },
+            },
+        });
 
+    }
+}
+continentsEL.forEach((continent) => {
+    continent.addEventListener("click", (e) => {
+        state.continent = e.target.dataset.continent;
+        getCountriesData(e.target.dataset.continent);
+        createChart(e.target.dataset.continent);
 
+    });
+});
 
-//create a doughnut chart for a country
+dataTypeEL.forEach((dataType) => {
+    dataType.addEventListener("click", (e) => {
+        state.dataType = e.target.dataset.type;
+        createChart(state.continent);
+    });
+});
 
-
-//check screen size, if not enough, display a message
-
-
-//extract data from storage and return it
+function dataTypeArr(dataType) {
+    return latest_dataArr.map((e) => e[dataType])
+}
